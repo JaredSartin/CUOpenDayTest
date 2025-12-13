c78a1bb89df55ab90673296d6760a31ecf803c96
Jared Sartin <jaredsartin@gmail.com>
2025-12-13 18:20:55 +0000

    Prevent tree-shaking of imported npm web component.

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: '/CUOpenDayTest/' // Change to your repo name
-})
+  base: "/CUOpenDayTest/", // Change to your repo name
+  build: {
+    rollupOptions: {
+      treeshake: {
+        moduleSideEffects: (id) =>
+          id.includes("slick-router/components/router-links.js"),
+      },
+    },
+  },
+});
