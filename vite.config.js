8c9a6f75a84c7f0051c87c179033253cbe5173d6
Jared Sartin <jaredsartin@gmail.com>
2025-12-13 18:24:38 +0000

    Revert "Prevent tree-shaking of imported npm web component."
    This reverts commit c78a1bb89df55ab90673296d6760a31ecf803c96.

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,5 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: "/CUOpenDayTest/", // Change to your repo name
-  build: {
-    rollupOptions: {
-      treeshake: {
-        moduleSideEffects: (id) =>
-          id.includes("slick-router/components/router-links.js"),
-      },
-    },
-  },
-});
+  base: '/CUOpenDayTest/' // Change to your repo name
+})
